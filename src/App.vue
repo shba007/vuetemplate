eb1d69c5ec0b6d7a39b04e4f98529ea504ea3d21
Shirsendu Bairagi <shirsendu2001@gmail.com>
2026-08-28 13:10:32 +0530

    init: Vue 3 project with Vite, TypeScript, and Pinia, Tailwind

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-slate-100">
+    <div class="p-6 max-w-sm bg-white rounded-xl shadow-lg space-y-4">
+      <h1 class="text-2xl font-bold text-slate-900">Vue + Tailwind v4</h1>
+      <p class="text-slate-500">It's up and running smoothly!</p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
